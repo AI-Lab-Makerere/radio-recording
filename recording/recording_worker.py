324af226e7118ed0527221ba86ed1f0994f0fb61
william <william.kibira@contango.no>
2020-10-21 20:11:04 +0300

    - Added the persistence query

--- recording/recording_worker.py
+++ recording/recording_worker.py
@@ -6,6 +6,8 @@ from datetime import datetime
 from threading import Thread
 from typing import List
 
+from sqlalchemy.orm import Session
+
 
 class RecordingConfiguration(object):
     def __init__(self, region: str, station: str, language: str, uri: str, folder: str):
@@ -17,7 +19,8 @@ class RecordingConfiguration(object):
 
 
 class ContentMeta(object):
-    def __init__(self, filepath: str, started_at: datetime, ended_at: datetime, size: int):
+    def __init__(self, filename: str, filepath: str, started_at: datetime, ended_at: datetime, size: int):
+        self.filename = filename
         self.filepath = filepath
         self.started_at = started_at
         self.ended_at = ended_at
@@ -25,25 +28,21 @@ class ContentMeta(object):
 
 
 class RecordingWorker(Thread):
-    def __init__(self, configuration: RecordingConfiguration):
+    def __init__(self, configuration: RecordingConfiguration, session: Session):
+        super().__init__(self)
+        self.__session = session
+        self.__is_running = True
         self.__configuration = configuration
 
-    def start(self):
-        pass
-
-    def stop(self):
-        pass
-
     def run(self) -> None:
-        pass
+        self.__record_audios(time_limit=300)
 
-    def __record_audios(self, time_limit: int):
+    def __record_audios(self, time_limit: int) -> None:
         while self.__is_running:
             start_time = datetime.now()
             generated_contents = self.__record_audio_files(start_time=start_time, time_limit=time_limit)
             for content in generated_contents:
-                tag_details = self.__tag_file(content=content)
-                self.__persist_file_information(information=tag_details)
+                self.__persist_file_information(information=content, station=self.__configuration.station)
 
     def __record_audio_files(self, start_time: datetime, time_limit: int) -> List[ContentMeta]:
         time_format = "%d-%m-%Y-T-%H:%M:%S"
@@ -70,9 +69,22 @@ class RecordingWorker(Thread):
                 end_timestamp = os.path.getmtime(absolute_filepath)
                 size = os.path.getsize(absolute_filepath)
                 generated_content.append(ContentMeta(
+                    filename=file,
                     filepath=absolute_filepath,
                     start_time=time.ctime(start_timestamp),
                     end_time=time.ctime(end_timestamp),
                     size=size
                 ))
             return generated_content
+
+    def __persist_file_information(self, information: ContentMeta, station: str) -> None:
+        # Database connection will be made here
+        # Call to persist the contents found in regards to the file in question
+        statement: str = "INSERT INTO recording_tb(station_id, name, created_at, finished_at, file_size) " \
+                         "VALUES(SELECT id FROM station_tb WHERE name=:station_name), :recording_name, :created_at, " \
+                         ":finished_at, :file_size)"
+        self.__session.execute(statement, params={'station_name': self.__configuration.station,
+                                                  'recording_name': information.filename,
+                                                  'created_at': information.started_at,
+                                                  'ended_at': information.ended_at}
+                               )
